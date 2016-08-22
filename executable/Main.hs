193ffdb69b389c0237016bf082c26da47f7dee4f
brady.ouren <brady.ouren@gmail.com>
2016-08-22 15:50:54 -0700

    fix time parser

--- executable/Main.hs
+++ executable/Main.hs
@@ -3,15 +3,14 @@ module Main (main) where
 -- import qualified TT
 import System.Environment
 import Text.Parsec
-import Data.Maybe
 
 type Parser = Parsec String ()
 
 data T = T { hours :: Int, minutes :: Int, seconds :: Int }
-  deriving Show
+  deriving (Eq)
 
--- instance Show T where
---   show (T h m s) = show (fromMaybe 0 h) ++ " hours " ++ show (fromMaybe 0 m) ++ " minutes " ++ show (fromMaybe 0 s) ++ " seconds"
+instance Show T where
+  show (T h m s) = show h ++ " hours " ++ show m ++ " minutes " ++ show s ++ " seconds"
 
 parse' :: Parser a -> String -> Either ParseError a
 parse' rule = parse rule "(source_file)"
@@ -20,9 +19,8 @@ parseArgs :: [String] -> T
 parseArgs [] = error "required time argument"
 parseArgs (a:_) = toTime a
 
-
 timeP :: Char -> Parser Int
-timeP c = valParser <* char c
+timeP c = option 0 $ try (valParser <* char c)
 
 timeParser :: Parser T
 timeParser = do
@@ -39,6 +37,13 @@ toTime t = case parse' timeParser t of
 valParser :: Parser Int
 valParser = rd <$> many1 digit
   where rd = read :: String -> Int
+
+spec :: IO ()
+spec = do
+  print $ show $ parseArgs ["1h22m3s"] == T 1 22 3
+  print $ show $ parseArgs ["2m3s"] == T 0 2 3
+  print $ show $ parseArgs ["2m"] == T 0 2 0
+  return ()
 
 main :: IO ()
 main = do -- TT.timeit
